70ba70afb24c28427511cca69d48cf3fdca30499
Matthew Burket <m@tthewburket.com>
2022-06-04 22:47:26 -0500

    Fix CI

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="3EhIcJlSwqd81NOBVkLtCTWCgvRmFsQEDoMezJd77UrsGwAjNfkh54vCak7YVAZA",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", '10.140.200.1']
 
 # CACHES
 # ------------------------------------------------------------------------------
